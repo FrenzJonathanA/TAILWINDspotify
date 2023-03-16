d5d7de4fc430fbaf82960517ee1325d1ea40c72e
unknown <popsgie>
2023-03-16 15:25:06 +0800

    spotify partial

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,20 @@
 const toggleMenu = document.querySelector(".toggle__menu");
 const headerNav = document.querySelector(".header__nav ul");
+const arJAY = document.querySelector(".arjay");
 toggleMenu.addEventListener("click", () => {
   toggleMenu.classList.toggle("open");
   headerNav.classList.toggle("open");
+  headerNav.style=("z-index:999");
   headerNav.style=("transition:.5s ease");
+  arJAY.classList.toggle("show");
 });
+
+const spotiFY = document.querySelectorAll(".item__top")
+const spot = document.querySelector(".item ul");
+spotiFY.forEach((premium) => {
+    premium.addEventListener("click", () => {
+        premium.nextElementSibling.classList.toggle("open");
+        premium.querySelector("i").classList.toggle("open");
+        spot.style=("transition:.5s ease");
+    })
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,12 @@ module.exports = {
         blue:'#1d75de',
         dark:'#000000',
         light: '#ffffff',
-        green: '#1db954'
+        green: '#1db954',
+        grayish: '#919496',
+        darkgreen: '#056952',
+        lightgreen: '#c5f0c9',
       }
+      
     },
     // container:{
     //   center:true,
